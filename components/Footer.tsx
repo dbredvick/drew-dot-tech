86ea5640f396581046fbfb0a93b5c6840d7f0331
Drew Bredvick <dbredvick@gmail.com>
2021-05-30 00:11:31 -0500

    oops removing dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,18 +28,6 @@ export const Footer: React.FC<{
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2021 {config.author}</div>
 
-      {hasMounted ? (
-        <div className={styles.settings}>
-          <a
-            className={styles.toggleDarkMode}
-            onClick={toggleDarkModeCb}
-            title='Tottle dark mode'
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        </div>
-      ) : null}
-
       <div className={styles.social}>
         {config.twitter && (
           <a
